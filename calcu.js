99d2169e252cff292654ef5189afc8ef318edad9
Sarfraz21 <71343478+Sarfraz21@users.noreply.github.com>
2020-09-15 23:22:34 +0530

    Delete calcu.js

--- calcu.js
+++
@@ -1,8 +0,0 @@
-let screen=document.getElementById('screen');
-button = document.querySelectorAll('button');
-for(item of buttons){
-    item.addEventListener('click',(e){
-        buttonText = e.target.innerText;
-        console.log('button text is', buttonText)
-    })
-}
